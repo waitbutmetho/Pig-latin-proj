1c5f19e2dba8cb40167cf954e06cf9e2ffdc17df
Joshua Gustafson and Michael Dada <silversevenfox@yahoo.com>
2016-01-20 15:27:52 -0800

    add functionality to pigLatin function for single word, added new specs

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,37 @@
 //
 // });
 var pigLatin = function(englishWord){
+  var splitAt = indexOfFirstVowel(englishWord);
+  return englishWord.slice(splitAt) + englishWord.slice(0, splitAt) + "ay";
+};
+
+var stringToArray = function(str){
+  return str.split(' ');
+};
+
+var hasConsonantAt = function(word, position) {
   var vowels = ['a', 'e', 'i', 'o', 'u'];
   for (var i = 0; i < vowels.length; i++) {
-    if (englishWord[0] === vowels[i]) {
-      var startsWithVowel = true;
-      break;
+    if (word[position] === vowels[i]) {
+      return false;
     }
   }
+  return true;
 
-    if (startsWithVowel){
-      var translatedWord = englishWord;
-    }else{
-    var translatedWord = englishWord.slice(1) + englishWord[0];
+};
+
+var indexOfFirstVowel = function(word) {
+  var index = -1;
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  var letters = word.split("");
+
+  for (var i = 0; i < letters.length; i++) {
+    var letter = letters[i];
+    for (var j = 0; j < vowels.length; j++) {
+      var vowel = vowels[i];
+      if (letters[i] === vowels[j]) {
+      return i;
+      }
     }
-    return translatedWord + 'ay';
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -25,10 +25,37 @@ describe("pigLatin", function(){
   it("takes two consonants from the beginning of a word and moves them to the end", function(){
     expect(pigLatin('cricket')).to.equal('icketcray');
   });
+
+
 });
 
-describe("vowelCheck", function(){
-  it("returns true if a word has a vowel in the specified spot", function(){
-    expect(vowelCheck("peanut", 1)).to.equal(true);
+describe("stringToArray", function(){
+  it("return an array consisting of each word in a string", function(){
+    expect(stringToArray("hello world")).to.eql(['hello','world']);
+  });
+});
+
+describe("arrayToPigLatin", function(){
+  it("takes array of english words and translates them each into pig latin", function(){
+    expect(arrayToPigLatin(["hello", "world"]).to.eql(["ellohay","orldway"]));
+  });
+});
+
+describe("hasConsonantAt", function(){
+  it("returns true if a word has a consonant in the specified spot", function(){
+    expect(hasConsonantAt("peanut", 0)).to.equal(true);
+  });
+  it("returns false if a word does not have a consonant in the specified spot", function(){
+    expect(hasConsonantAt("peanut", 1)).to.equal(false);
+  });
+});
+
+describe("indexOfFirstVowel", function(){
+  it("returns 0 for a word that starts with a vowel", function(){
+    expect(indexOfFirstVowel("apple")).to.equal(0);
+  });
+
+  it("returns 1 for a word that starts with a single consonant", function(){
+    expect(indexOfFirstVowel("cat")).to.equal(1);
   });
 });
