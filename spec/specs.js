772ed4094f134adf325ed8fd2e1e867813f87b45
Joshua Gustafson and Michael Dada <silversevenfox@yahoo.com>
2016-01-20 13:28:42 -0800

    add spec for pigLatin

--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,19 @@
         //Examples
-        
-describe("leapYear", function(){
-  it("returns false for a year that is not divisible by 4, 100, or 400", function() {
-    expect(leapYear(1993)).to.equal("false");
-  });
-});
 
-
-describe("triangleTracker", function(){
-  it("return equilateral if all sides are equal", function() {
-    expect(triangleTracker(2, 2, 2)).to.equal("equilateral");
+// describe("leapYear", function(){
+//   it("returns false for a year that is not divisible by 4, 100, or 400", function() {
+//     expect(leapYear(1993)).to.equal("false");
+//   });
+// });
+//
+//
+// describe("triangleTracker", function(){
+//   it("return equilateral if all sides are equal", function() {
+//     expect(triangleTracker(2, 2, 2)).to.equal("equilateral");
+//   });
+// });
+describe("pigLatin", function(){
+  it("adds 'ay' to the end of a word that starts with a vowel", function(){
+    expect(pigLatin('apple')).to.equal('appleay');
   });
 });
